bad733cd4d214deff2479c0cdb3e15bb42d9ebc7
pindellk <pindellk@protonmail.com>
2020-11-03 21:02:53 -0500

    Fix file path line 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
-const generateMarkdown = require(".utils/generateMarkdown")
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // array of questions for user
 const questions = [
